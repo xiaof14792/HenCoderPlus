314de45daf9911a870104ea9c8dd627881453b86
fangxiang <xiaof14792@gmail.com>
2020-07-01 17:39:38 +0800

    新增MotionLayout「https://juejin.im/post/5d595328f265da03c34bfa59」Sample

---
+++ constraintsample/src/main/java/com/example/constraintsample/MultiBallActivity.kt
@@ -0,0 +1,12 @@
+package com.example.constraintsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MultiBallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multi_balls)
+
+    }
+}
